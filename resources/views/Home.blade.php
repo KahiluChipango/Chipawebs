1ff1ece263bbfa336362d51204c27aa3f5b3e726
Kahilu Chipango <35219186+KahiluChipango@users.noreply.github.com>
2023-03-22 17:22:51 +0200

    added some staff

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -6,7 +6,7 @@
             <div class="md:flex md:items-center md:justify-between">
                 <div class="flex items-center justify-between">
                     <div class="text-xl font-semibold text-gray-100">
-                        <a class="no-underline text-gray-100" href="#">My Website</a>
+                    <a class="no-underline text-gray-100" href="{{ route('home') }}">My Website</a>
                     </div>
 
 
@@ -31,7 +31,7 @@
 
                 <!-- Facebook icon -->
                 <div class="flex items-center">
-                    <a href="https://facebook.com/chipawebz" class="text-gray-100 hover:text-gray-400 focus:outline-none focus:text-gray-400" aria-label="Facebook">
+                    <a href="https://facebook.com/chipawebz" target="_blank" class="text-gray-100 hover:text-gray-400 focus:outline-none focus:text-gray-400" aria-label="Facebook">
                         <svg class="h-6 w-6 fill-current" viewBox="0 0 24 24">
                             <path fill-rule="evenodd" clip-rule="evenodd" d="M12 2.251c-5.493 0-9.972 4.479-9.972 9.972 0 4.938 3.59 9.012 8.307 9.764v-6.905h-2.406v-2.706h2.406v-1.98c0-2.382 1.422-3.688 3.574-3.688 1.035 0 1.965.077 2.223.112v2.47l-1.516.001c-1.194 0-1.424.567-1.424 1.396v1.829h2.844l-.372 2.706h-2.472v6.905c4.717-.752 8.307-4.826 8.307-9.764-.001-5.493-4.479-9.972-9.971-9.972z"/>
                         </svg>
@@ -44,7 +44,7 @@
 
     <!-- Content -->
     <div class="container mx-auto py-8">
-        <h1 class="text-3xl font-bold text-center">Welcome to my website</h1>
+        <h1 class="text-3xl font-bold text-center">Chipa Webs</h1>
     </div>
 
     <!-- Scripts -->
--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('Home');
-});
+})->name('home');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
